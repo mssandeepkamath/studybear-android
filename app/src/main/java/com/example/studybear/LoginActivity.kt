04775d3ace21e25c2ed51549b82615715b9330e3
mssandeepkamath <msandeepcip@gmail.com>
2022-05-07 01:18:39 +0530

    login ui updated

---
+++ app/src/main/java/com/example/studybear/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.studybear
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+
+class LoginActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/studybear/SetUpActivity.kt
@@ -0,0 +1,18 @@
+package com.example.studybear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+
+class SetUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+        fun statusBarSetUp()
+        {
+
+        }
+
+}

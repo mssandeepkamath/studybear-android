4820803b91a2d8d716bb459a22d759f81d0f698b
msandeepk <90695071+mssandeepkamath@users.noreply.github.com>
2023-04-17 16:15:05 +0530

    stable

--- app/src/main/java/com/sandeep/studybear/activity/activity/PdfActivity.kt
+++ app/src/main/java/com/sandeep/studybear/activity/activity/PdfActivity.kt
@@ -42,7 +42,6 @@ import com.sandeep.studybear.R
          lateinit var mAdView:AdView
      }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
